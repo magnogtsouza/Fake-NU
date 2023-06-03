0ffe1499357e67b192de9e94feaefe7364632f5d
Magno Souza <magnogtsouza@outlook.com>
2023-06-02 21:46:29 -0300

    correções de bugs

--- Fake NU/ConfigPage.xaml.cs
+++ Fake NU/ConfigPage.xaml.cs
@@ -5,5 +5,42 @@ public partial class ConfigPage : ContentPage
 	public ConfigPage()
 	{
 		InitializeComponent();
-	}
+        DisplayAlert("Atenção", "O intuito deste aplicativo é a criação de materiais no github para ajudar outros devs, e tambem ultilizar como uma brincadeira simulando um valor ficticio na conta.Este app não deve ser ultilizado para aplicar golpes.", "OK");
+        DisplayAlert("Direitos autorais", "O layout foi inspirado no app nubank da playstore, os icones são do mesmo app e tambem de \"fonts.google.com/icons", "OK");
+        lblConta.Text = Memory.debit.ToString("C");
+        lblCredit.Text = Memory.credit.ToString("C");
+        lblFatura.Text = Memory.fatura.ToString("C");
+        slConta.Maximum = Memory.maxDebit;
+        slCredit.Maximum = Memory.maxCredit;
+        slFatura.Maximum = Memory.maxCredit;
+        slConta.Value = Memory.debit;
+        slCredit.Value = Memory.credit;
+        slFatura.Value = Memory.fatura;
+        slConta.ValueChanged += SlValueChanged;
+        slCredit.ValueChanged += SlValueChanged;
+        slFatura.ValueChanged += SlValueChanged;
+    }
+
+    private void SaveData()
+    {
+        Memory.debit = slConta.Value;
+        Memory.credit = slCredit.Value;
+        Memory.fatura = slFatura.Value;
+        //await DisplayAlert("Sucesso", "Dados salvos com sucesso", "OK");
+        //await Navigation.PopModalAsync();
+    }
+    private async void Updateform()
+    {
+        lblConta.Text = slConta.Value.ToString("C");
+        lblCredit.Text = slCredit.Value.ToString("C");
+        lblFatura.Text = slFatura.Value.ToString("C");
+    }
+
+    private void SlValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        Updateform();
+        SaveData();
+    }
+
+  
 }
--- Fake NU/MainPage.xaml.cs
+++ Fake NU/MainPage.xaml.cs
@@ -27,11 +27,16 @@ public partial class MainPage : ContentPage
     }
     private async void ImageButton_Clicked(object sender, EventArgs e)
     {
-        await DisplayAlert("Atenção", "O intuito deste aplicativo é a criação de materiais no github para ajudar outros devs, e tambem ultilizar como uma brincadeira simulando um valor ficticio na conta.Este app não deve ser ultilizado para aplicar golpes.", "OK");
-        await DisplayAlert("Direitos autorais", "O layout foi inspirado no app nubank da playstore, os icones são do mesmo app e tambem de \"fonts.google.com/icons", "OK");
-        await Navigation.PushModalAsync(new ConfigPage());
-
+        //Shell.Current.GoToAsync("ConfigPage");
+        //NavigationPage
+        ConfigPage s = new ConfigPage();
+        Navigation.PushModalAsync(s);
+        while (s.Parent != null)
+        {
+            await Task.Delay(100);
+        }
         UpdateInfo();
     }
+
 }
 
